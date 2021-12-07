7905bfddd464bbaac843d4ca4001af7ac251186a
Erin <etruman2@gmail.com>
2021-12-07 12:54:20 -0600

    some progress on reaction and reviewer views

--- halpapi/views/community_resource_view.py
+++ halpapi/views/community_resource_view.py
@@ -53,19 +53,6 @@ class ComunityResourceView(ViewSet):
 
 
 
-# class UserSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = User
-#         fields = ('id', 'first_name', 'last_name', 'username')
-
-
-# class ReviewerSerializer(serializers.ModelSerializer):
-#     user = UserSerializer()
-
-#     class Meta:
-#         model = Reviewer
-#         fields = ('id', 'user', 'profile_pic', 'is_admin' )
 
 
 class Community_Resource_Serializer(serializers.ModelSerializer):
