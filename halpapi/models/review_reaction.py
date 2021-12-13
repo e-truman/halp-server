7a78c59efc377fb30c9f72d44cde40edd8d980b6
Erin <etruman2@gmail.com>
2021-12-13 15:13:55 -0600

    added user to model and fixtures for review_reactions

--- halpapi/models/review_reaction.py
+++ halpapi/models/review_reaction.py
@@ -4,4 +4,9 @@ from django.db import models
 class Review_Reaction(models.Model):
     reaction = models.ForeignKey("Reaction", on_delete=models.CASCADE)
     review = models.ForeignKey("Review", on_delete=models.CASCADE)
+    reviewer = models.ForeignKey("Reviewer", on_delete=models.CASCADE, default=False)
    
+
+
+
+  
