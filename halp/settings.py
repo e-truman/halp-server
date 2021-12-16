2ceb3cbe269a38d8762ed508cf91e8ce1c0399d6
Erin <etruman2@gmail.com>
2021-12-16 11:48:49 -0600

    added front end web address to settings.py

--- halp/settings.py
+++ halp/settings.py
@@ -67,7 +67,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://halp-client.herokuapp.com'
 )
 
 ROOT_URLCONF = 'halp.urls'
